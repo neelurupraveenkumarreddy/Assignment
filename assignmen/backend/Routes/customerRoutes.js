2fe619887ecea907e2f155a193b3d8440accaf87
Neeluru Praveen Kumar Reddy <130646483+neelurupraveenkumarreddy@users.noreply.github.com>
2025-07-26 10:52:22 +0530

    Delete assignmen directory

--- assignmen/backend/Routes/customerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const customerController=require("../Controllers/customerController");
-router.get("/all",customerController.getAllCustomers
-);
-router.get("/:customer_id",customerController.getCustomerName);
-router.post("/create",customerController.createCustomer);
-router.get("/loan_summary/:customer_id",customerController.getCustomerLoanSummary);
-module.exports=router;
--- assignmen/backend/Routes/loanRoutes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const loanController=require("../Controllers/loanController");
-
-router.get("/ledger/:loan_id",loanController.LoanLedger);
-router.post("/create",loanController.createLoan);
-router.post("/payments/:loan_id",loanController.recordPayment);
-module.exports=router;
